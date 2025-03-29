3486769097a4b182ed7eae536dd5f3f5695d5e13
Nafiz-71 <swacha2006@gmail.com>
2025-03-29 17:25:20 -0400

    removed response.json in route.ts of queries

--- app/query/route.ts
+++ app/query/route.ts
@@ -13,13 +13,14 @@
  	return data;
  }
 
-//export async function GET() {
+ export async function GET() {
   //return Response.json({
-  //  message:
-    //  'Uncomment this file and remove this line. You can delete this file when you are finished.',
+   // message:
+     // 'Uncomment this file and remove this line. You can delete this file when you are finished.',
   //});
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+    try {
+    	return Response.json(await listInvoices());
+    } catch (error) {
+    	return Response.json({ error }, { status: 500 });
+    }
+}
